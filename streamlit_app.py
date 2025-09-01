6ffd9e951e8b98ca762b12822ebc054aad5c27f3
Eder Herrera <eder.herrera@motivus.com>
2025-09-01 13:48:15 -0600

    Connected changes due to moving from SiS to SinS
    Connected changes due to moving from SiS to SinS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 from snowflake.snowpark.functions import when_matched
 
@@ -11,7 +10,8 @@ st.write(
   """
 )
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.ORDERS").filter(col("ORDER_FILLED")==0).collect()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
